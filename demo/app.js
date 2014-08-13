e1ca954727293a54ab3a2e9a62a9e4380384cc70
jeremy.derusse <jeremy.derusse@sensiolabs.com>
2014-08-14 00:48:31 +0200

    Migrate to bootsrap

--- demo/app.js
+++ demo/app.js
@@ -5,46 +5,30 @@ angular.module('app', ['exResource'])
     $scope.user = User.get({id: 123});
     $scope.user.$promise.then(function(user) {
       $xResourceConfig.prefix = 'u/' + user.id + '/';
+      $scope.customers = Customer.query();
       $scope.customer2 = Customer.get({id: 2});
       $scope.customer5 = Customer.get({id: 5});
       $scope.customer6 = Customer.get({id: 6});
-      $scope.customers = Customer.query();
     });
     $scope.localStorage = $window.localStorage;
   })
   .factory('Customer', function($xResource) {
+    var cacheConfig = {
+      key: 'customer/:id',
+      split: {
+        'addresses': {
+          key: 'customer/:id/address/:addressId',
+          params: {'addressId': '@id'}
+        },
+        'addresses.country': {
+          key: 'country/:code',
+          params: {'code': '@code'}
+        },
+      }
+    };
     return $xResource('./fixtures/customers/:id.json', {id: '@id'}, {
-      get: {method:'GET', $accessProperty: 'customer', $cache:{
-        key: 'customer/:id',
-        split: {
-          'addresses': {
-            key: 'customer/:id/address/:addressId',
-            params: {'addressId': '@id'}
-          },
-          'addresses.country': {
-            key: 'country/:code',
-            params: {'code': '@code'}
-          },
-        }
-      }},
-      query: {method:'GET', isArray: true, $accessProperty: 'customers', $cache:{
-        key: 'customers',
-        split: {
-          '': {
-            key: 'customer/:id',
-            split: {
-              'addresses': {
-                key: 'customer/:id/address/:addressId',
-                params: {'addressId': '@id'}
-              },
-              'addresses.country': {
-                key: 'country/:code',
-                params: {'code': '@code'}
-              }
-            }
-          }
-        }
-      }},
+      get: {method:'GET', $accessProperty: 'customer', $cache: cacheConfig},
+      query: {method:'GET', isArray: true, $accessProperty: 'customers', $cache:{key: 'customers', split: {'': cacheConfig}}}
     });
   })
   .factory('User', function($xResource) {
